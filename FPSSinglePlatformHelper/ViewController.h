d650d1002a81267bc49733747b239c4a4faa493c
echolush <echolush@echolushs-Mac-Pro.local>
2013-12-09 12:06:32 -0800

    Initial Commit

---
+++ FPSSinglePlatformHelper/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FPSSinglePlatformHelper
+//
+//  Created by echolush on 12/9/13.
+//  Copyright (c) 2013 Frames Per Sound. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FPSSinglePlatformHelper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FPSSinglePlatformHelper
+//
+//  Created by echolush on 12/9/13.
+//  Copyright (c) 2013 Frames Per Sound. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
